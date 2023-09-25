851bd5a7000321e5a02a90c52be37ed64ad64da5
Shivan Singh <shivan.0972@gmail.com>
2023-09-25 08:29:22 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,70 @@
+from flask import Flask, render_template, request, Response
+import cv2
+ 
+app = Flask(__name__)
+ 
+video = cv2.VideoCapture('static/output/abc.mp4') #// if you have second camera you can set first parameter as 1
+face_cascade = cv2.CascadeClassifier()
+face_cascade.load(cv2.samples.findFile("static/haarcascade_frontalface_alt.xml")) 
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+VIDEO_EXTENSIONS = ['mp4']
+PHOTO_EXTENSIONS = ['png','jpeg','jpg']
+ 
+def fextension(filename):
+    return filename.rsplit('.', 1)[1].lower()
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'video' not in request.files:
+        return 'No video file found'
+    file = request.files['video']
+    if file.filename == '':
+        return 'No video selected'
+    if file:
+        exttype=fextension(file.filename)
+        print(exttype)
+        if exttype in VIDEO_EXTENSIONS:
+            file.save('static/input/videos/' + file.filename)
+            print('video')
+            return render_template('preview_video.html', file_name=file.filename, type='video/'+exttype)
+        elif exttype in PHOTO_EXTENSIONS:
+            file.save('static/input/photo/' + file.filename)
+            print('photo')
+            return render_template('preview_photo.html', file_name=file.filename, type='image/'+exttype)
+    return 'Invalid video file'
+
+def gen(video):
+    while True:
+        success, image = video.read()
+        frame_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        frame_gray = cv2.equalizeHist(frame_gray)
+
+        faces = face_cascade.detectMultiScale(frame_gray)
+
+        for (x, y, w, h) in faces:
+            center = (x + w//2, y + h//2)
+            cv2.putText(image, "X: " + str(center[0]) + " Y: " + str(center[1]), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+            image = cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+            faceROI = frame_gray[y:y+h, x:x+w]
+        ret, jpeg = cv2.imencode('.jpg', image)
+
+        frame = jpeg.tobytes()
+        
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    global video
+    # if not (video.isOpened()):
+    #     return 'Could not connect to camera'
+    return Response(gen(video),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
